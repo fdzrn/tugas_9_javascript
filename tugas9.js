4c35604f846ad2711ab8a85a890d5f5049b6384d
fdzrn <fadzriangibran@gmail.com>
2019-07-31 15:00:59 +0700

    Tugas 9 javascript
    Tugas ini saya kirimkan kpd niomic

---
+++ tugas9.js
@@ -0,0 +1,13 @@
+function tugasSembilan() {
+	let biodata = {
+		nama: 'Udin',
+		umur: 44,
+		domisili: 'Ciamis',
+		kawin: true,
+		pekerjaan: 'Buruh'
+	}
+	for( biodataNew in biodata ) {
+		console.log(biodata[biodataNew]);
+	}
+}
+tugasSembilan();
